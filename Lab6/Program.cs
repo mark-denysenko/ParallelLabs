e5f942836814cd9350d6eb325217d7ad2a3d34b1
Mark <mark.denyssenko@gmail.com>
2020-04-03 00:01:59 +0300

    added mpi lab

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MPI;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,14 +15,48 @@ namespace Lab6
         {
             MPI.Environment.Run(ref args, comm =>
             {
-                Console.WriteLine($"Hello world from <{comm.Rank}> from {comm.Size}");
+                if (comm.Rank == 0)
+                {
+                    var random = new Random(DateTime.UtcNow.Millisecond);
+                    var vector = Enumerable.Range(0, 10_000_000).Select(_ => random.NextDouble()).ToList();
+
+                    int partitionSize = (int)Math.Ceiling((double)vector.Count / (comm.Size - 1));
+                    for (int dest = 1; dest < comm.Size; ++dest)
+                    {
+                        comm.Send(vector.Skip((dest - 1) * partitionSize).Take(partitionSize).ToList(), dest, 0);
+                    }
+
+                    double sum = 0.0;
+                    for (int dest = 1; dest < comm.Size; ++dest)
+                    {
+                        sum += comm.Receive<double>(dest, 1);
+                    }
+
+                    Console.WriteLine("Euclidean Norm : " + Math.Sqrt(sum));
+                }
+                else
+                {
+                    var subVector = comm.Receive<IEnumerable<double>>(0, 0);
+                    var subSum = subVector.Sum(v => v * v);
+                    comm.Send(subSum, 0, 1);
+                }
 
                 //PingPong(comm);
             });
 
         }
 
+        public static double EuclideanNormCalculate(IEnumerable<double> vector)
+        {
+            double sum = 0d;
 
+            foreach (var number in vector)
+            {
+                sum += number * number;
+            }
+
+            return Math.Sqrt(sum);
+        }
 
         private static void PingPong(Intracommunicator comm)
         {
